7a9ac059fa166b937c6de3eecb5b7e1416ece513
bpcivball <seldeny076@gmail.com>
2026-04-15 00:40:05 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  stroke(255, 100); // Faint white lines
+  translate(width / 2, height); // Start at the bottom center
+  drawBranch(100); // Start drawing the "tree"
+}
+
+function drawBranch(len) {
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if (len > 4) {
+    push();
+    rotate(map(mouseX, 0, width, 0, PI/2)); // Mouse moves the branches!
+    drawBranch(len * 0.67);
+    pop();
+    push();
+    rotate(-map(mouseX, 0, width, 0, PI/2));
+    drawBranch(len * 0.67);
+    pop();
+  }
+}
